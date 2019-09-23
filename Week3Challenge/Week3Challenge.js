1804dd43f47adcefae83240a19413e434f27bfb1
unknown <gurvir14128@gmail.com>
2019-09-23 11:46:27 -0400

    Uploading Week3 Challenge

---
+++ Week3Challenge/Week3Challenge.js
@@ -0,0 +1,67 @@
+window.onload = function () {
+    document.getElementById("fetchUsers").addEventListener("click", getRandomUsers);
+}
+
+let apiUrl = 'https://randomuser.me/api/?results=25';
+let state = { users: '' };
+let ul = document.getElementById('randomUser');
+let myDiv = document.getElementById('container');
+let myTable = document.getElementById('myTable');
+let age = 0;
+
+function createEle(ele) {
+    return document.createElement(ele); 
+  }
+
+  function addToEle(ele1, ele2) {
+    return ele1.appendChild(ele2); 
+  }
+
+  function getRandomUsers() {
+    fetch(apiUrl, {method:"GET", headers:{"Accept":"application/json"}})
+
+    .then((response) => {
+        let data = response.json();
+        return data;
+
+    }) 
+
+    .then(function(data) {
+        let users = data.results; 
+        //console.log(users);
+        let uLength = users.length;
+        let myRow = createEle('tr'); 
+        addToEle(myTable, myRow);
+        for(let i = 0; i < users.length; i++) { 
+        let myDiv2 = createEle('td'); 
+        let img = createEle('img');
+        let p = createEle('p');
+        img.src = users[i].picture.thumbnail; 
+        p.innerHTML = `Name: ${users[i].name.first} ${users[i].name.last} Age: ${users[i].dob.age}`; 
+        
+        if( i % 4 !== 0) {
+            addToEle(myDiv2, img);
+            addToEle(myDiv2, p);
+            addToEle(myRow, myDiv2);
+
+        } else {
+            myRow = createEle.apply('tr');
+            addToEle(myTable, myRow);
+            addToEle(myDiv2, img);
+            addToEle(myDiv2, p);
+            addToEle(myRow, myDiv2);
+
+        }
+        //console.log(rUser.dob.age);
+        age = age + users[i].dob.age
+        //console.log(age);
+      }
+        //console.log(uLength);
+        let avg = age / uLength;
+        let p2 = createEle('p');
+        p2.innerHTML = `Average Age of Users: ${avg}`;
+        addToEle(myDiv, p2);
+
+    })
+
+  }
